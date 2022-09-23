1038412383b5ff0eecaa6c470933acae863d43fc
MohamedShokrani <MohamedShokrani55@gmail.com>
2022-09-23 17:07:41 +0200

    First Commit

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using DatingApp.Data;
+using DatingApp.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _Context;
+
+        public UsersController(DataContext Context)
+        {
+            _Context = Context;
+        }
+        [HttpGet]
+         //public IActionResult then we specify the type of thing that we gonna return inside this result  <> 
+         // IEnu,erable allows us to use simple iteration of a collection 
+         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var  users =await _Context.Users.ToListAsync();
+            if (users is null)
+            {
+                return Ok("A7A no users can be find");
+            }
+            return Ok(users);
+
+
+        }
+        [HttpGet("{id}")]
+        public async Task< ActionResult<IEnumerable<AppUser>>> GetUsersById(int id)
+        {
+            var user =await _Context.Users.FindAsync(id);   
+            if (user is null)
+            {
+                return Ok("A7A no users can be find");
+            }
+            return Ok(user);
+
+
+        }
+
+    };
+}
---
+++ DatingApp/Entity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace DatingApp.Entity
+{
+    public class AppUser
+    {
+        public string? UserName { get; set; }
+       // public string? Password { get; set; }
+        public int id { get; set; }
+
+
+    }
+}
