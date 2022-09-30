2caded07b163f24887c0553471e98f7e0069aaab
MohamedShokrani <MohamedShokrani55@gmail.com>
2022-10-01 00:17:05 +0200

    Adding cors to enable angluar to use the API

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -17,6 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(connectionString));
 
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
@@ -38,7 +39,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials()
+    .WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthentication(); // must come before use authorization
